09cbb03a9034ca1f2fe0e059c4223e4c7bdd1054
rbgir1113-dot <rbgir1113@gmail.com>
2026-04-02 13:54:02 +0900

    feat: spring_day02

--- mybatis/src/main/java/com/app/mybatis/mapper/MemberMapper.java
+++ mybatis/src/main/java/com/app/mybatis/mapper/MemberMapper.java
@@ -4,9 +4,18 @@ import com.app.mybatis.domain.vo.MemberVO;
 import org.apache.ibatis.annotations.Mapper;
 
 import java.util.List;
+import java.util.Optional;
 
 @Mapper
-public interface MemberMapper {
+public interface MemberMapper {     // RDB 중심설계
     public List<MemberVO> selectAll();
 
+    public Optional<MemberVO> select(Long id);
+
+    public void insert(MemberVO memberVO);
+
+    public void update(MemberVO memberVO);
+
+    public void delete(Long id);
 }
+
--- mybatis/src/test/java/com/app/mybatis/mapper/MapperTests.java
+++ mybatis/src/test/java/com/app/mybatis/mapper/MapperTests.java
@@ -1,9 +1,14 @@
 package com.app.mybatis.mapper;
 
+import com.app.mybatis.domain.vo.MemberVO;
+import com.app.mybatis.domain.vo.PostVO;
 import lombok.extern.slf4j.Slf4j;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+
+import javax.sound.midi.Sequence;
+import java.util.Optional;
 
 @SpringBootTest
 @Slf4j
@@ -14,19 +19,107 @@ public class MapperTests {
 
     @Autowired
     private MemberMapper memberMapper;
+    @Autowired
+    private MemberVO memberVO;
+    @Autowired
+    private PostMapper postMapper;
 
     @Test
-    public void MapperTests() {
+    public void mapperTests() {
         log.info(timeMapper.getTime());
     }
 
     @Test
-    public void MapperTests2() {
+    public void mapperTests2() {
         log.info(timeMapper.getTime2());
     }
 
     @Test
-    public void MapperTests3() {
+    public void mapperTests3() {
         log.info(memberMapper.selectAll().toString());
+    }
+
+    @Test
+    public void mapperTest4() {
+        memberMapper.select(2L).map(MemberVO::toString).ifPresent(log::info);
+    }
+
+    @Test
+    public void insertTest() {
+        MemberVO memberVO = new MemberVO();
+//        memberVO.setMemberEmail("test789@gmail.com");
+//        memberVO.setMemberPassword("test123!@#");
+//        memberVO.setMemberName("장보고");
+
+        memberVO.setMemberEmail("test456@gmail.com");
+        memberVO.setMemberPassword("test123!@#");
+        memberVO.setMemberName("이순신");
+
+        memberMapper.insert(memberVO);
+
+    }
+
+    @Test
+    public void updateTest() {
+        MemberVO memberVO = new MemberVO();
+        memberVO.setId(2L);
+        memberVO.setMemberEmail("test456@gmail.com");
+        memberVO.setMemberPassword("test123!@#");
+        memberVO.setMemberName("장길동");
+
+        memberMapper.select(2L).map(MemberVO::toString).ifPresent(log::info);
+        memberMapper.update(memberVO);
+        memberMapper.select(2L).map(MemberVO::toString).ifPresent(log::info);
+    }
+
+    @Test
+    public void deleteTest() {
+        memberMapper.delete(2L);
+    }
+
+    @Test
+    public void postInsertTest() {
+        PostVO postVO = new PostVO();
+        postVO.setPostTitle("개미는 뚠뚠 오늘도 뚠뚠");
+        postVO.setPostContent("열심히 일을 하네 뚠뚠");
+        postVO.setMemberId(3L);
+        postVO.setPostReadCount(5L);
+        postMapper.insert(postVO);
+    }
+
+    @Test
+    public void postSelectAllTest() {
+        postMapper.selectAll().forEach(postVO -> log.info(postVO.toString()));
+    }
+
+    @Test
+    public void postSelectTest() {
+        postMapper.select(8L).map(PostVO::toString).ifPresent(log::info);
+    }
+
+
+
+    @Test
+    public void postSelectPostReadCountTest() {
+        postMapper.selectPostReadCount().forEach(postVO -> log.info(postVO.toString()));
+    }
+
+    @Test
+    public void updatePostTest() {
+        PostVO postVO = new PostVO();
+        postVO.setId(8L);
+        postVO.setPostTitle("개미는 뚠뚠 오늘도 뚠뚠");
+        postVO.setPostContent("열심히 일을 하네 뚠뚠");
+        postVO.setMemberId(4L);
+        postVO.setPostReadCount(12L);
+
+        postMapper.select(8L).map(PostVO::toString).ifPresent(log::info);
+        postMapper.update(postVO);
+        postMapper.select(8L).map(PostVO::toString).ifPresent(log::info);
+    }
+
+    @Test
+    public void deletePostTest() {
+        postMapper.delete(7L);
     }
 }
